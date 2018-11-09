3785f3bdfef6f5ea1e1350a34fd94072ba24e257
Isabell (Issy) Long <me@issyl0.co.uk>
2018-11-09 16:02:43 +0000

    The name test works now

--- events/app_watcher_test.go
+++ events/app_watcher_test.go
@@ -2,54 +2,51 @@
 package events
 
 import (
-  "log"
+	"log"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-  "github.com/cloudfoundry-community/go-cfclient"
-  "github.com/prometheus/client_golang/prometheus"
-
-//  sonde_events "github.com/cloudfoundry/sonde-go/events"
-
+	"github.com/cloudfoundry-community/go-cfclient"
+	"github.com/prometheus/client_golang/prometheus"
+	//  sonde_events "github.com/cloudfoundry/sonde-go/events"
 )
 
 var _ = Describe("AppWatcher", func() {
+	var (
+		appWatcher *AppWatcher
+		// Apps        []cfclient.App
+		registerer prometheus.Registerer
+	)
 
+	BeforeEach(func() {
 
-  BeforeEach(func() {
-    var (
-      AppWatcher *AppWatcher
-      // Apps        []cfclient.App
-      registerer prometheus.Registerer
-    )
+		config := &cfclient.Config{
+			ApiAddress:        "some/endpoint",
+			SkipSslValidation: true,
+			Username:          "barry",
+			Password:          "password",
+			ClientID:          "dummy_client_id",
+			ClientSecret:      "dummy_client_secret",
+		}
 
-    config := &cfclient.Config{
-      ApiAddress:        "some/endpoint",
-      SkipSslValidation: true,
-      Username:          "barry",
-      Password:          "password",
-      ClientID:          "dummy_client_id",
-      ClientSecret:      "dummy_client_secret",
-    }
+		apps := []cfclient.App{
+			{Guid: "33333333-3333-3333-3333-333333333333", Instances: 1, Name: "foo", SpaceURL: "/v2/spaces/123"},
+		}
 
-    apps := []cfclient.App{
-      {Guid: "33333333-3333-3333-3333-333333333333", Instances: 1, Name: "foo", SpaceURL: "/v2/spaces/123"},
-    }
+		log.Printf("app: %v", apps)
+		log.Printf("app: %v", config)
+		registerer = prometheus.DefaultRegisterer
+		appWatcher = NewAppWatcher(config, apps[0], registerer)
 
-    log.Printf("app: %v", apps)
-    log.Printf("app: %v", config)
-    registerer = prometheus.DefaultRegisterer
-    AppWatcher = NewAppWatcher(config, apps[0], registerer)
+		log.Printf("app: %v", appWatcher)
 
-    log.Printf("app: %v", AppWatcher)
+	})
+	AfterEach(func() {})
 
-  })
-  AfterEach(func() {})
-
-  Describe("AppName", func() {
-    It("knows the name of its application", func() {
-      Expect(AppWatcher.AppName()).To(Equal("foo"))
-    })
-  })
+	Describe("AppName", func() {
+		It("knows the name of its application", func() {
+			Expect(appWatcher.AppName()).To(Equal("foo"))
+		})
+	})
 })
