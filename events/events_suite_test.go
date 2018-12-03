998a27fab5abd05231a5d0f8d430753a1041e848
David McDonald <idavidmcdonald@me.com>
2018-12-03 11:23:44 +0000

    Only print logs if test fails
    Means we don't get lots of unhelpful log output such as repeated
    calls for "checking for new apps". Will still log if a test fails.
    
    https://godoc.org/github.com/onsi/ginkgo#pkg-variables

--- events/events_suite_test.go
+++ events/events_suite_test.go
@@ -2,12 +2,14 @@ package events_test
 
 import (
 	"testing"
+	"log"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 func TestEvents(t *testing.T) {
+	log.SetOutput(GinkgoWriter)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Events Suite")
 }
